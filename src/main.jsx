57394f1bb2f3ea98f16b1aedd707e5a0abf7a132
Rushikesh Sanjay Kharche <rkrushikesh98@gmail.com>
2026-09-06 15:38:06 +0530

    Deploy React Vite project

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   
-<BrowserRouter basename="/Rushi-PortFolio">
+<BrowserRouter >
   <App />
 </BrowserRouter>
 )
